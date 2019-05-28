b3f632d240a8e92e0bad83fd4ed2fff22462123b
Riyderzq1 <50485040+Riyderzq1@users.noreply.github.com>
2019-05-29 02:12:47 +0300

    Add files via upload

---
+++ bot.js
@@ -0,0 +1,68 @@
+﻿const Discord = require('discord.js');
+const client = new Discord.Client();
+const prefix = '$'
+
+client.on('ready', () => {
+    console.log(`~~~~~~~~~~~~~~~~~`);
+    console.log(`Logging into Discord`);
+    console.log(`~~~~~~~~~~~~~~~~~~~~~`);
+    console.log(`on  ${client.guilds.size} Servers `);
+    console.log(`~~~~~~~~~~~~~~~~~~~~~~~~`);
+    console.log(`Logged in as ${client.user.tag}!`);
+    client.user.setGame(`#TheH1ts clan|$Help`,"http://twitch.tv/y04zgamer")
+    client.user.setStatus("dnd")
+ });
+ 
+
+
+
+
+
+
+
+
+
+
+
+client.on('message', message => {
+    var prefix = "$";
+    
+        if (message.author.id === client.user.id) return;
+        if (message.guild) {
+       let embed = new Discord.RichEmbed()
+        let args = message.content.split(' ').slice(1).join(' ');
+    if(message.content.split(' ')[0] == prefix + 'bc') {
+        if (!args[1]) {
+    message.channel.send("**f!bc <message>**");
+    return;
+    }
+            message.guild.members.forEach(m => {
+       if(!message.member.hasPermission('ADMINISTRATOR')) return;
+                var bc = new Discord.RichEmbed()
+                .addField('» السيرفر :', `${message.guild.name}`)
+                .addField('» المرسل : ', `${message.author.username}#${message.author.discriminator}`)
+                .addField(' » الرسالة : ', args)
+                .setColor('#ff0000')
+                // m.send(`[${m}]`);
+                m.send(`${m}`,{embed: bc});
+            });
+        }
+        } else {
+            return;
+        }
+    });
+    
+
+
+
+
+
+
+
+
+
+
+
+
+
+client.login("NTc5NTkyMTE3OTcwNjY1NDc1.XOupBg._ljCkkyrb8BwJZDp3pPEKrBx5Oo");
